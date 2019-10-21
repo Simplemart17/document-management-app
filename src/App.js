fc3596bbd5bc8228469aa8b50d024ebfbea70687
Martins Aloba <aloba.martins@gmail.com>
2019-10-21 13:57:39 +0100

    project setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <p>
+        Testing out the feature
+      </p>
+    </header>
+  </div>
+);
+
+export default App;
